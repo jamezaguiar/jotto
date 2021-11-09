dea71fb8f4175b4e41e1565b9630b7683f2a7e61
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-09 20:38:56 -0300

    render component function

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -1,6 +1,16 @@
 import { shallow } from 'enzyme';
 import Congrats from './Congrats';
 
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function renderComponent
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const renderComponent = (props = {}) => {
+  return shallow(<Congrats {...props} />);
+};
+
 describe('<Congrats />', () => {
   it('should renders without error', () => {});
 
