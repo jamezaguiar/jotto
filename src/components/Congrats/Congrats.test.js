c004bb8655a0d3837ef4bff4408854e007fcda40
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-09 20:49:34 -0300

    writing Congrats component tests

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -13,9 +13,21 @@ const renderComponent = (props = {}) => {
 };
 
 describe('<Congrats />', () => {
-  it('should renders without error', () => {});
+  it('should renders without error', () => {
+    const wrapper = renderComponent();
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.length).toBe(1);
+  });
 
-  it('should renders no text when `success` prop is false', () => {});
+  it('should renders no text when `success` prop is false', () => {
+    const wrapper = renderComponent({ success: false });
+    const component = findByTestAttr(wrapper, 'component-congrats');
+    expect(component.text()).toBe('');
+  });
 
-  it('should renders non-empty congrats message when `success` prop is true', () => {});
+  it('should renders non-empty congrats message when `success` prop is true', () => {
+    const wrapper = renderComponent({ success: true });
+    const message = findByTestAttr(wrapper, 'component-message');
+    expect(message.text().length).not.toBe(0);
+  });
 });
