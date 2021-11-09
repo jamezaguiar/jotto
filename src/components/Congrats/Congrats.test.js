42655b34d0fa6aff4ed39a6521ed81d57583705e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-09 20:34:14 -0300

    test cases

--- src/components/Congrats/Congrats.test.js
+++ src/components/Congrats/Congrats.test.js
@@ -3,4 +3,8 @@ import Congrats from './Congrats';
 
 describe('<Congrats />', () => {
   it('should renders without error', () => {});
+
+  it('should renders no text when `success` prop is false', () => {});
+
+  it('should renders non-empty congrats message when `success` prop is true', () => {});
 });
