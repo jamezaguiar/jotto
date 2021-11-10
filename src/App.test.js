9fb6e7311047e7e5ef2b53b9627988cc7bd733ac
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-10 10:52:14 -0300

    Using components in App.js, styling App.js with bootstrap

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders page title `Jotto`', () => {
+  const wrapper = shallow(<App />);
+  const pageTitle = wrapper.find('h1').text();
+  expect(pageTitle).toBe('Jotto');
 });
