9bf589d8ca402e1bd5a6f392fe781fc3fa186e5a
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-10 09:43:21 -0300

    GuessedWords 'if there are words guessed' testing

--- src/components/GuessedWords/GuessedWords.js
+++ src/components/GuessedWords/GuessedWords.js
@@ -9,7 +9,25 @@ const GuessedWords = ({ guessedWords }) => {
           Try to guess the secret word!
         </span>
       ) : (
-        <table></table>
+        <div data-test="guessed-words">
+          <h3>Guessed Words</h3>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching Letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {guessedWords.map((word, index) => (
+                <tr key={index} data-test="guessed-word">
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
