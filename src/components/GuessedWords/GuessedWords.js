9a8e117c420109fd6bdaedc0b2752ca0ff26b64e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-10 09:28:10 -0300

    writing GuessedWords component

--- src/components/GuessedWords/GuessedWords.js
+++ src/components/GuessedWords/GuessedWords.js
@@ -2,7 +2,17 @@ import React from 'react';
 import { arrayOf, number, shape, string } from 'prop-types';
 
 const GuessedWords = ({ guessedWords }) => {
-  return <div />;
+  return (
+    <div data-test="component-guessed-words">
+      {guessedWords.length === 0 ? (
+        <span data-test="guess-instructions">
+          Try to guess the secret word!
+        </span>
+      ) : (
+        <table></table>
+      )}
+    </div>
+  );
 };
 
 GuessedWords.propTypes = {
