1da88054daf83ccecdd9e8c04a17a33ba2e50f33
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-10 09:17:03 -0300

    using describe and beforeEach to write tests

--- src/components/GuessedWords/GuessedWords.test.js
+++ src/components/GuessedWords/GuessedWords.test.js
@@ -17,8 +17,26 @@ const renderComponent = (props = {}) => {
   return shallow(<GuessedWords {...setupProps} />);
 };
 
-describe('<GuessedWords />', () => {
+describe('propTypes testing', () => {
   it('should not throw warning with expected props', () => {
     checkProps(GuessedWords, defaultProps);
+  });
+});
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = renderComponent({ guessedWords: [] });
+  });
+
+  it('should renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  it('should renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
   });
 });
