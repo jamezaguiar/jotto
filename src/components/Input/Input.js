1d972551f2f8ae2afd74980bd730f3ef8e60f225
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-11-11 08:27:19 -0300

    testing props

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { string } from 'prop-types';
 
-const Input = () => {
+const Input = ({ secretWord }) => {
   return <div data-test="component-input"></div>;
+};
+
+Input.propTypes = {
+  secretWord: string.isRequired,
 };
 
 export default Input;
--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -1,6 +1,8 @@
 import { shallow } from 'enzyme';
-import { findByTestAttr } from '../../utils/testUtils';
+import { checkProps, findByTestAttr } from '../../utils/testUtils';
 import Input from './Input';
+
+const defaultProps = { secretWord: 'train' };
 
 /**
  * Factory function to create a ShallowWrapper for the Input component.
@@ -9,11 +11,16 @@ import Input from './Input';
  * @returns {ShallowWrapper}
  */
 const renderComponent = (props = {}) => {
-  return shallow(<Input {...props} />);
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Input {...setupProps} />);
 };
 
 it('should renders without error', () => {
   const wrapper = renderComponent();
   const component = findByTestAttr(wrapper, 'component-input');
   expect(component.length).toBe(1);
+});
+
+it('should not throw warning with expected props', () => {
+  checkProps(Input, defaultProps);
 });
