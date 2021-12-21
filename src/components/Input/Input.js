74190bb4db21f07103c01bb09c797b6ba54d9aed
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-12-21 20:01:18 -0300

    testing form rendering for success prop

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { string } from 'prop-types';
+import { bool, string } from 'prop-types';
 
-const Input = ({ secretWord }) => {
+const Input = ({ secretWord, success }) => {
   const [currentGuess, setCurrentGuess] = useState('');
 
   const handleSubmit = event => {
@@ -13,30 +13,33 @@ const Input = ({ secretWord }) => {
 
   return (
     <div data-test="component-input">
-      <form className="form-inline">
-        <input
-          data-test="input-box"
-          className="mb-2 mx-sm-3"
-          type="text"
-          placeholder="enter guess"
-          value={currentGuess}
-          onChange={event => setCurrentGuess(event.target.value)}
-        />
-        <button
-          data-test="submit-button"
-          className="btn btn-primary mb-2"
-          type="submit"
-          onClick={handleSubmit}
-        >
-          Submit
-        </button>
-      </form>
+      {!success && (
+        <form className="form-inline">
+          <input
+            data-test="input-box"
+            className="mb-2 mx-sm-3"
+            type="text"
+            placeholder="enter guess"
+            value={currentGuess}
+            onChange={event => setCurrentGuess(event.target.value)}
+          />
+          <button
+            data-test="submit-button"
+            className="btn btn-primary mb-2"
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
+      )}
     </div>
   );
 };
 
 Input.propTypes = {
   secretWord: string.isRequired,
+  success: bool.isRequired,
 };
 
 export default Input;
--- src/components/Input/Input.test.js
+++ src/components/Input/Input.test.js
@@ -8,7 +8,7 @@ jest.mock('react', () => ({
   useState: initialState => [initialState, mockSetState],
 }));
 
-const defaultProps = { secretWord: 'train' };
+const defaultProps = { secretWord: 'train', success: false };
 
 /**
  * Factory function to create a ShallowWrapper for the Input component.
@@ -21,14 +21,52 @@ const renderComponent = (props = {}) => {
   return shallow(<Input {...setupProps} />);
 };
 
-it('should renders without error', () => {
-  const wrapper = renderComponent();
-  const component = findByTestAttr(wrapper, 'component-input');
-  expect(component.length).toBe(1);
-});
-
 it('should not throw warning with expected props', () => {
   checkProps(Input, defaultProps);
+});
+
+describe('render', () => {
+  it('should renders without error', () => {
+    const wrapper = renderComponent();
+    const component = findByTestAttr(wrapper, 'component-input');
+    expect(component.length).toBe(1);
+  });
+
+  describe('when success is true', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = renderComponent({ success: true });
+    });
+
+    it('should not show input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.exists()).toBeFalsy();
+    });
+
+    it('should not show submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.exists()).toBeFalsy();
+    });
+  });
+
+  describe('when success is false', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = renderComponent({ success: false });
+    });
+
+    it('should show input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.exists()).toBeTruthy();
+    });
+
+    it('should show submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.exists()).toBeTruthy();
+    });
+  });
 });
 
 describe('state controlled input field', () => {
