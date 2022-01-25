ed1b36eb03f45fa195919d787745fca909758b34
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2022-01-25 08:37:22 -0300

    testing useEffect action call

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 
 import Congrats from './components/Congrats/Congrats';
 import Input from './components/Input/Input';
 import GuessedWords from './components/GuessedWords/GuessedWords';
 
+import { getSecretWord } from './actions';
+
 function App() {
   // TODO: get props from shared state
   const success = false;
   const secretWord = 'party';
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div className="container" data-test="component-app">
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,18 @@
-import { shallow } from 'enzyme';
+/* eslint-disable import/first */
+jest.mock('./actions');
+
+import { mount } from 'enzyme';
 import { findByTestAttr } from './utils/testUtils';
 import App from './App';
+import { getSecretWord as mockGetSecretWord } from './actions';
 
 const defaultProps = {};
 
 const renderComponent = (props = {}) => {
+  // using mount, because useEffect aren't called on `shallow`
+  // https://github.com/enzymejs/enzyme/issues/2086
   const setupProps = { ...defaultProps, ...props };
-  return shallow(<App {...setupProps} />);
+  return mount(<App {...setupProps} />);
 };
 
 it('should renders without error', () => {
@@ -19,4 +25,26 @@ it('should renders page title `Jotto`', () => {
   const wrapper = renderComponent();
   const pageTitle = findByTestAttr(wrapper, 'page-title');
   expect(pageTitle.text()).toBe('Jotto');
+});
+
+describe('get secret word', () => {
+  beforeEach(() => {
+    mockGetSecretWord.mockClear();
+  });
+
+  it('should get secret word on app mount', () => {
+    const wrapper = renderComponent();
+
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(1);
+  });
+
+  it('should not run getSecretWord on app update', () => {
+    const wrapper = renderComponent();
+    mockGetSecretWord.mockClear();
+
+    // using setProps because wrapper.update() doesn't trigger useEffect
+    // https://github.com/enzymejs/enzyme/issues/2254
+    wrapper.setProps();
+    expect(mockGetSecretWord).toHaveBeenCalledTimes(0);
+  });
 });
